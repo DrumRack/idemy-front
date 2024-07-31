bd243d57051479ebe23872bd8c10f065832dae01
Dexter <redrumrack@gmail.com>
2024-07-31 20:46:34 +0300

    Переход изменён с window.location на хук useNavigate

--- src/components/shared/Content.jsx
+++ src/components/shared/Content.jsx
@@ -2,26 +2,6 @@ import Button from '../ui/button/Button'
 
 function Content({topics, getPicLinks, picsLinks, setModalVisible}) {
     return (
-        // <main>
-        //     {picsLinks.length
-        //     ? <nav>
-        //         {topics.map(topic =>
-        //             <Button onClick={() => getPicLinks(topic)} key={topic}>
-        //                 {topic}
-        //             </Button>)}
-        //     </nav>
-        //     : <nav></nav>}
-        //     <div className='content'>
-        //         {picsLinks.length
-        //             ? <img src={`http://95.163.242.46:8000/${picsLinks[0]}`} alt="Content image"/>
-        //             : <div style={{display: 'flex', justifyContent: 'center'}}>
-        //                 {topics.map(topic =>
-        //                     <Button onClick={() => getPicLinks(topic)} key={topic}>
-        //                         {topic}
-        //                     </Button>)}
-        //             </div>}
-        //     </div>
-        // </main>
         <main>
             <div className='topic-nav'>
                 {picsLinks.length
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,17 @@
-import {Link} from 'react-router-dom'
+import {useNavigate, Link} from 'react-router-dom'
 import Button from '../ui/button/Button'
 
 function Header() {
+    const navigate = useNavigate()
+
     return (
         <header>
-            {/* Поменять на useNavigate после изучения хука */}
-            <img src='https://csite.nicepage.com/Images/logo-w.png' onClick={() => window.location='/'} style={{cursor: 'pointer'}} alt='Logo'></img>
+            <img
+                src='https://csite.nicepage.com/Images/logo-w.png'
+                className='logo'
+                onClick={() => navigate('/')}
+                alt='Logo'
+            />
             <nav className='header__main-nav'>
                 <Button>
                     <Link to='/quiz'>Тест знаний</Link>
